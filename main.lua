63f12edaf4bc4d7099dfa7fa419efc4586bf34b9
Luan Fagioni <101909254+DevLuanFagioni@users.noreply.github.com>
2023-04-04 00:27:07 -0300

    adicionando arquivos ✅

---
+++ main.lua
@@ -0,0 +1,8 @@
+
+local composer = require('composer')
+
+composer.gotoScene( 'cenas.loading' )
+
+
+display.setDefault( 'background', 185/255, 153/255, 130/255)
+display.setStatusBar( display.HiddenStatusBar )
